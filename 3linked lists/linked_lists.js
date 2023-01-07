fa7c9c12325d7df49d4d4c7f79b7c08fe4c1f7b0
Ning Li <lining04111223@gmail.com>
2023-01-07 14:48:19 -0800

    insert to linked lists

--- 3linked lists/linked_lists.js
+++ 3linked lists/linked_lists.js
@@ -7,31 +7,60 @@ class LinkedList {
 
     this.tail = this.head;
     this.length = 1;
-    console.log("this", this);
+    //console.log("this", this);
   }
   append(value) {
+    //0(1)
     const newNode = {
       value: value,
       next: null,
     };
     this.tail.next = newNode;
-    console.log("this2", this);
+    //this.head.next = newNode;//same with previous line
+    //console.log("this2", this);
     this.tail = newNode;
+    //this.head = newNode; //not same
     this.length++;
-    console.log("this3", this);
+    //console.log("this3", this);
     return this;
   }
   prepend(value) {
+    //O(1)
     const newNode = {
       value: value,
       next: null,
     };
     newNode.next = this.head;
-    console.log("this4", this);
+    //console.log("newnode", newNode);
+    //console.log("this4", this);
     this.head = newNode;
-    console.log("this5", this);
+    //console.log("this5", this);
     this.length++;
     return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      //console.log(currentNode);
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    const leader = this.traverseToIndex(index - 1);
   }
 }
 
@@ -39,4 +68,6 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
+myLinkedList.insert(100, 99);
+console.log(myLinkedList.printList());
