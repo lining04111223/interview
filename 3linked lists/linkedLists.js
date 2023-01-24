a6bc3ae7f46b1f443bc044a3875ef3cafc159c3d
Ning Li <lining04111223@gmail.com>
2023-01-23 22:20:56 -0800

    Linkedlist

---
+++ 3linked lists/linkedLists.js
@@ -0,0 +1,86 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let couter = 0;
+    let currentNode = this.head;
+    while (couter !== index) {
+      currentNode = currentNode.next;
+      couter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(99, 4);
+myLinkedList.insert(2, 40);
+myLinkedList.remove(1);
+console.log(myLinkedList.printList());
